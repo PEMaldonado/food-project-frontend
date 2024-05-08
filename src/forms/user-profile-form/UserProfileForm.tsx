f69cc88ba33dbf3f2f44790ac3267f9f686915c3
PEMaldonado <pablomaldonado2086@gmail.com>
2024-05-08 11:51:56 -0300

    first commit

---
+++ src/forms/user-profile-form/UserProfileForm.tsx
@@ -0,0 +1,146 @@
+import LoadingButton from "@/components/LoadingButton";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { User } from "@/types";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+//* Definimos un objeto que representa al formulario, y los elementos que va a contener dicho formulario.
+//* También las validaciones
+const formSchema = z.object({
+  email: z.string().optional(),
+  name: z.string().min(1, "Name is required"),
+  addressLine1: z.string().min(1, "AddressLine1 is required"),
+  country: z.string().min(1, "Cuntry is required"),
+  city: z.string().min(1, "City is required"),
+});
+
+//* Se define un tipo de objeto. En este caso se utiliza la libreria zod para inferir el tipo de objeto que
+//* se definió previamente. Es decir, UserFormData va a tener la forma de formSchema.
+type UserFormData = z.infer<typeof formSchema>;
+
+//* Se define otro tipo para las props que recibira el componente de react.
+//* Este tendrá un metodo onSave que recibirá un formulario con el tipo definido previamente(UserFormData)
+//* Y devolverá void (vacío/nada/undefined)
+//* Y un booleano isLoading.
+type Props = {
+  currentUser: User;
+  onSave: (userProfileData: UserFormData) => void;
+  isLoading: boolean;
+};
+
+const UserProfileForm = ({ onSave, isLoading, currentUser }: Props) => {
+  //* Se llama al hook de react-hook-form useForm, y se aclara que el formulario será del tipo UserFormData.
+  //*
+  const form = useForm<UserFormData>({
+    //* El resolver es el que va a hacer las validaciones y todo lo relacionado a la librería de zod
+    resolver: zodResolver(formSchema),
+    defaultValues: currentUser,
+  });
+
+  useEffect(() => {
+    form.reset(currentUser);
+  }, [currentUser, form]);
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSave)}
+        className="space-y-4 bg-gray-50 rounded-lg md:p-10"
+      >
+        <div className="">
+          <h2 className="text-2xl font-bold">User Profile Form</h2>
+          <FormDescription>
+            View and change your profile information here
+          </FormDescription>
+        </div>
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input {...field} disabled className="bg-white" />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input {...field} className="bg-white" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="flex flex-col md:flex-row gap-4">
+          <FormField
+            control={form.control}
+            name="addressLine1"
+            render={({ field }) => (
+              <FormItem className="flex-1">
+                <FormLabel>Address Line 1</FormLabel>
+                <FormControl>
+                  <Input {...field} className="bg-white" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="city"
+            render={({ field }) => (
+              <FormItem className="flex-1">
+                <FormLabel>City</FormLabel>
+                <FormControl>
+                  <Input {...field} className="bg-white" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="country"
+            render={({ field }) => (
+              <FormItem className="flex-1">
+                <FormLabel>Country</FormLabel>
+                <FormControl>
+                  <Input {...field} className="bg-white" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        {isLoading ? (
+          <LoadingButton />
+        ) : (
+          <Button type="submit" className="bg-orange-500">
+            Submit
+          </Button>
+        )}
+      </form>
+    </Form>
+  );
+};
+
+export default UserProfileForm;
