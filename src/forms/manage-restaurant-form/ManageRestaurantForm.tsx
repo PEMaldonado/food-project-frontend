f99ae4af6112a3c4d5213086581fa14356fd3b42
PEMaldonado <pablomaldonado2086@gmail.com>
2024-05-13 11:49:11 -0300

    added restaurant-management

---
+++ src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -0,0 +1,161 @@
+import { Form } from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import DetailsSection from "./DetailsSection";
+import { Separator } from "@/components/ui/separator";
+import CuisinesSection from "./CuisinesSection";
+import MenuSection from "./MenuSection";
+import ImageSection from "./ImageSection";
+import LoadingButton from "@/components/LoadingButton";
+import { Button } from "@/components/ui/button";
+import { Restaurant } from "@/types";
+import { useEffect } from "react";
+
+const formSchema = z
+  .object({
+    restaurantName: z.string({
+      required_error: "Restaurant name is required",
+    }),
+    city: z.string({
+      required_error: "City is required",
+    }),
+    country: z.string({
+      required_error: "Country is required",
+    }),
+    deliveryPrice: z.coerce.number({
+      required_error: "Delivery price is required",
+      invalid_type_error: "Must be a valid number",
+    }),
+    estimatedDeliveryTime: z.coerce.number({
+      required_error: "Estimated delivery time is required",
+      invalid_type_error: "Must be a valid number",
+    }),
+    cuisines: z.array(z.string()).nonempty({
+      message: "Please select at least one item",
+    }),
+    menuItems: z.array(
+      z.object({
+        name: z.string().min(1, "Name is required"),
+        price: z.coerce.number().min(1, "Price is required"),
+      })
+    ),
+    imageUrl: z.string().optional(),
+    imageFile: z.instanceof(File, { message: "Image is required" }).optional(),
+  })
+  .refine((data) => data.imageUrl || data.imageFile, {
+    message: "Either image URL or image File must be provided",
+    path: ["imageFile"],
+  });
+
+type RestaurantFormData = z.infer<typeof formSchema>;
+
+type Props = {
+  restaurant?: Restaurant;
+  onSave: (restaurantFormData: FormData) => void;
+  isLoading: boolean;
+};
+
+const ManageRestaurantForm = ({ restaurant, onSave, isLoading }: Props) => {
+  const form = useForm<RestaurantFormData>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      cuisines: [],
+      menuItems: [{ name: "", price: 0 }],
+    },
+  });
+
+  useEffect(() => {
+    if (!restaurant) {
+      return;
+    }
+
+    const deliveryPriceFormatted = parseInt(
+      (restaurant.deliveryPrice / 100).toFixed(2)
+    );
+
+    const menuItemsFormatted = restaurant.menuItems.map((item) => ({
+      ...item,
+      price: parseInt((item.price / 100).toFixed(2)),
+    }));
+
+    const updatedRestaurant = {
+      ...restaurant,
+      deliveryPrice: deliveryPriceFormatted,
+      menuItems: menuItemsFormatted,
+    };
+
+    form.reset(updatedRestaurant);
+  }, [form, restaurant]);
+
+  const onSubmit = (formDataJson: RestaurantFormData) => {
+    //* Esto se hace para convertir el objeto Json que se obtiene del proceso que hace react-hook-form
+    //* a un formData, de manera que sea multipart, es decir, que también puede contener archivos (en este caso la imagen)
+
+    //* Se crea el formData
+    const formData = new FormData();
+
+    //*Se le agregan todas las cosas
+    formData.append("restaurantName", formDataJson.restaurantName);
+    formData.append("city", formDataJson.city);
+    formData.append("country", formDataJson.country);
+
+    formData.append(
+      "deliveryPrice",
+      //* Esto se hace para obtener el valor de moneda de menor denominacion. Supongamos que el precio es 1 peso con cincuenta centavos,
+      //* nosotros lo convertiremos a 150 centavos (es un ejemplo, nadie se maneja en centavos en arg hoy en día.)
+      (formDataJson.deliveryPrice * 100).toString()
+    );
+
+    formData.append(
+      "estimatedDeliveryTime",
+      formDataJson.estimatedDeliveryTime.toString()
+    );
+
+    //* Esto se enviará como
+    //* cuisines[0] = Pizza
+    //* cuisines[1] = Hamburguesa
+    formDataJson.cuisines.forEach((cuisine, index) => {
+      formData.append(`cuisines[${index}]`, cuisine);
+    });
+
+    //* Esto se enviará como
+    //* menuItems[0] = Pizza 4 quesos
+    //* menuItems[0] = $1 (que convertido serían 100 centavos)
+    //* menuItems[1] = Hamburguesa completa
+    //* menuItems[1] = $5 (que convertido serían 500 centavos)
+    formDataJson.menuItems.forEach((menuItem, index) => {
+      formData.append(`menuItems[${index}][name]`, menuItem.name);
+      formData.append(
+        `menuItems[${index}][price]`,
+        (menuItem.price * 100).toString()
+      );
+    });
+
+    if (formDataJson.imageFile) {
+      formData.append("imageFile", formDataJson.imageFile);
+    }
+
+    onSave(formData);
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 bg-gray-50 p-10 rounded-lg"
+      >
+        <DetailsSection />
+        <Separator />
+        <CuisinesSection />
+        <Separator />
+        <MenuSection />
+        <Separator />
+        <ImageSection />
+        {isLoading ? <LoadingButton /> : <Button type="submit">Submit</Button>}
+      </form>
+    </Form>
+  );
+};
+
+export default ManageRestaurantForm;
